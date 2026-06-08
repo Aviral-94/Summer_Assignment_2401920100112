1facd147fdf499b41a4a57324501916642e79049
Aviral-94 <gaviral2246@gmail.com>
2026-06-08 09:45:47 +0530

    Added Week 2 Day 2 DSA solutions

---
+++ Week_2/Day_1/RansomNote.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    bool canConstruct(string ransomNote, string magazine) {
+        unordered_map<char,int>mp;
+
+        for (int i = 0; i< ransomNote.size();i++)
+        {
+            mp[ransomNote[i]]++;
+        }
+        for (int i = 0; i< magazine.size();i++)
+        {
+            if (mp.find(magazine[i]) != mp.end())mp[magazine[i]]--;
+        }
+
+        for (auto it : mp)
+        {
+            if (it.second >= 1)return false;
+        }
+        return true;
+    }
+};
