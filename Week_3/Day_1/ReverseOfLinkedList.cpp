b438a09db9e5e6f5ab7589a4bfe6ba2abc0a70b5
Aviral-94 <gaviral2246@gmail.com>
2026-06-15 10:05:28 +0530

    Added Week 3 Day 1 solution

---
+++ Week_3/Day_1/ReverseOfLinkedList.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    ListNode* reverseList(ListNode* head) {
+
+        ListNode* curr = head;
+        ListNode* prev = NULL;
+
+        while (curr != NULL) {
+            ListNode* nextNode = curr->next; 
+
+            curr->next = prev; 
+
+            prev = curr;      
+            curr = nextNode;   
+        }
+
+        return prev;
+    }
+};
