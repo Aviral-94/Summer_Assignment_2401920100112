8b9960432f5170eeb3d4dbf49b277c37ffe54334
Aviral-94 <gaviral2246@gmail.com>
2026-06-24 09:46:33 +0530

    Added Day 3 solution of week 4

---
+++ Week_4/Day_3/ValidateBinarySearchTree.cpp
@@ -0,0 +1,26 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+   bool helper (TreeNode* root , TreeNode* max , TreeNode* min)
+   {
+    if (root == NULL)return true;
+    if (min && min -> val >= root->val)return false;
+    if (max && max -> val <= root -> val)return false;
+
+    return helper (root -> left ,root,min ) &&
+    helper (root -> right,max,root);
+   }
+    bool isValidBST(TreeNode* root) {
+       return helper (root,NULL,NULL);
+    }
+};
